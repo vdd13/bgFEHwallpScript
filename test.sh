ef5ded552e7e4fc3387662202183856487406ca4
vdd13 <58014317+vdd13@users.noreply.github.com>
2021-11-24 20:52:49 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,15 +8,18 @@
 
 #ls -al Downloads/*.jpg | wc -l 
 
-
+#count .jpg files i.e. 7
 SIZE=$(echo $(ls -l Downloads/*.jpg | wc -l))
 #echo $SIZE
 
+#pick up random number from 1 to SIZE
 RAND=$(shuf -i 1-$SIZE -n 1)
 #echo $RAND
 
+#pick up .jpg file name based on RAND number
 IMAGE=$( ls Downloads/*.jpg | sed "${RAND}q;d" )
 #echo $IMAGE
 
+#set up .feh file
 echo "#!/bin/bash" > .fehbg
 echo "feh --no-fehbg --bg-scale '/home/vdd13/$IMAGE'" >> .fehbg
